96d3e5784d3bff43f301c45b3d1bf3c006f5a9f3
unknown <mikey@jigsaw.xyz>
2019-01-07 10:49:16 -0500

    i hate css

--- src/components/workout/index.js
+++ src/components/workout/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Transition } from 'semantic-ui-react';
+
 import Edit from './images/edit.png';
 import View from './images/view.png';
 
@@ -9,38 +11,56 @@ class Workout extends Component {
     super(props);
     
     this.state = {
-      collapsed: true,
+      collapsed: false,
     };
   };
 
   handleEditToggle = () => {
+    const collapsed = !this.state.collapsed;
+
     this.setState({
-      collapsed: !this.state.collapsed,
+      collapsed,
     });
   };
 
   handleViewToggle = () => {
+    const collapsed = !this.state.collapsed; 
+
     this.setState({
-      collapsed: !this.state.collapsed,
+      collapsed,
     });
   };
 
+  showCollapsableElements = () => {
+    return (
+      <form style={{ animationName: 'expand', animationDuration: '.5s', animationFillMode: 'forwards' }}>
+        <label name="completed">{ 'completed' }</label><br />
+        <input type="text"></input>
+      </form>
+    );
+  };
+
+  hideCollapsableElements = () => {
+    return (
+      <form className="workout--container__collapsed">
+      </form>
+    );
+  }
+
   render(){
     const { workout } = this.props;
+    const { collapsed } = this.state;
 
     return (
       <div className="workout--container">
+      <div className="workout--container__title-container">
         <h2 className="workout--container__title">{ workout.day }</h2>
-        <div className="workout--container__icons">
+        <div style={{ textAlign: 'right' }} className="workout--container__icons">
           <img src={Edit} alt="edit" onClick={ () => { this.handleEditToggle() } } />
           <img src={View} alt="view" onClick={ () => { this.handleViewToggle() } } />
         </div>
-        { !this.state.collapsed &&
-          <form>
-            <label></label>
-            <input type="text"></input>
-          </form>
-        }
+      </div>
+      { collapsed ? this.showCollapsableElements() : this.hideCollapsableElements() }
       </div>
     );
   };
