008bcbe11419a312cf8b49113173eabeeb4a6f09
unknown <mikey@jigsaw.xyz>
2019-01-09 00:30:54 -0500

    styled tray items

--- src/components/tray-item/index.js
+++ src/components/tray-item/index.js
@@ -3,14 +3,14 @@ import React from 'react';
 import './styles.scss';
 
 const TrayItem = ({week}) => {
-  const { date, finished, expected } = week;
+  const { date, finished, expected, workouts } = week;
 
   return (
-    <div className="tray-item--container">
+    <div style={{ borderColor: finished && 'lightgreen' }} className="tray-item--container">
       <h4>{ date }</h4>
       <p>{ `finished: ${finished}` }</p>
       <p>{ `expected miles: ${expected}` }</p>
-      <p>{ `completed: `}</p>
+      <p>{ `runs: ${workouts.length}`}</p>
     </div>
   );
 }
