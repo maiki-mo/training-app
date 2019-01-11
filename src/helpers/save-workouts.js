4a08699bb025e33acb33a87648a4fa9f6ae2f0af
unknown <mikey@jigsaw.xyz>
2019-01-11 10:39:03 -0500

    kinda working with server now

--- src/helpers/save-workouts.js
+++ src/helpers/save-workouts.js
@@ -1,6 +1,15 @@
-import * as fs from 'fs';
+import axios from 'axios';
 
-export const saveWorkoutData = (workouts) => {
-  console.log(workouts);
-  console.log(fs);
+export const saveWorkoutData = async (workouts) => {
+  await axios({
+    url: 'http://localhost:3001/save-workouts/',
+    method: 'POST',
+    data: workouts,
+  })
+  .then((res) => {
+    console.log(res.data);
+  })
+  .catch((res) => {
+    console.log('Err:', err);
+  });
 }
